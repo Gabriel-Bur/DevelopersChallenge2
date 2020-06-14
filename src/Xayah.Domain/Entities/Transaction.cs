c2fc843bef35fcfc8dac35e798fa46144a8f5396
gabriel.bur@outlook.com <gabriel.bur@outlook.com>
2020-06-14 13:58:37 -0300

    chages in domain properties

--- src/Xayah.Domain/Entities/Transaction.cs
+++ src/Xayah.Domain/Entities/Transaction.cs
@@ -7,12 +7,12 @@ namespace Xayah.Domain.Entities
     {
         [Key]
         public Guid Id { get; set; }
-        public int AccountId { get; set; }
-        public int BankId { get; set; }
+        public long AccountId { get; set; }
+        public long BankId { get; set; }
         public string TransactionCurrency { get; set; }
         public string TransactionType { get; set; }
         public DateTime TranscationDateTime { get; set; }
-        public int TransactionAmmount { get; set; }
+        public double TransactionAmmount { get; set; }
         public string TransactionDescription { get; set; }
     }
 }
